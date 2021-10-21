dbbd8bda0f16865aba8cb4d2c3e78fbeede02da8
AngelDX <angel.condori@gmail.com>
2021-10-21 10:19:20 -0500

    api v1

---
+++ apirest/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    nombres=models.CharField(max_length=50)
+    apellidos=models.CharField(max_length=50)
+    dni=models.CharField(max_length=8)
+    celular=models.CharField(max_length=20)
+    email=models.EmailField(max_length=50)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = ("Cliente")
+        verbose_name_plural = ("Clientes")
+
+    def __str__(self):
+        return self.nombres
